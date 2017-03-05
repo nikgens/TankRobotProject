825f3ffbd9c262b1b17974209b1135dcffe52b14
Gennadiy S. Nikitin <gsnikitin@gmail.com>
2017-03-05 09:39:42 +0000

    script changes

--- signRecognition/signRecognition.py
+++ signRecognition/signRecognition.py
@@ -1,132 +1,99 @@
-# USAGE
-# python recognize_digits.py
-
-# import the necessary packages
 from imutils.perspective import four_point_transform
 from imutils import contours
 import imutils
 import cv2
+import numpy as np
 
-# define the dictionary of digit segments so we can identify
-# each digit on the thermostat
+# define the dictionary of signs segments so we can identify
+# each signs on the image
 SIGNS_LOOKUP = {
-	(1, 0, 0, 1): turnRight,
-	(0, 0, 1, 1): turnLeft,
-	(0, 1, 0, 0): moveStraight,
-	(1, 0, 1, 1): turnBack
+        (1, 0, 0, 1): 'turnRight', # turnRight
+        (0, 0, 1, 1): 'turnLeft', # turnLeft
+        (0, 1, 0, 0): 'moveStraight', # moveStraight
+        (1, 0, 1, 1): 'turnBack', # turnBack
 }
 
 # load the example image
-image = cv2.imread("example.jpg")
+image = cv2.imread("turnLeft.png")
 
-# pre-process the image by resizing it, converting it to
-# graycale, blurring it, and computing an edge map
-image = imutils.resize(image, height=500)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(blurred, 50, 200, 255)
+def defineTrafficSign(image):
 
-# find contours in the edge map, then sort them by their
-# size in descending order
-cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
-displayCnt = None
+        # pre-process the image by resizing it, converting it to
+        # graycale, blurring it, and computing an edge map
+        image = imutils.resize(image, height=500)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        edged = cv2.Canny(blurred, 50, 200, 255)
 
-# loop over the contours
-for c in cnts:
-	# approximate the contour
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        # find contours in the edge map, then sort them by their
+        # size in descending order
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+                cv2.CHAIN_APPROX_SIMPLE)
+        # The is_cv2() and is_cv3() are simple functions that can be used to 
+        # automatically determine the OpenCV version of the current environment
+        # cnts[0] or cnts[1] hold contours
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+        displayCnt = None
 
-	# if the contour has four vertices, then we have found
-	# the thermostat display
-	if len(approx) == 4:
-		displayCnt = approx
-		break
+        # loop over the contours
+        for c in cnts:
+                # approximate the contour
+                peri = cv2.arcLength(c, True)
+                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
-# extract the thermostat display, apply a perspective transform
-# to it
-warped = four_point_transform(gray, displayCnt.reshape(4, 2))
-output = four_point_transform(image, displayCnt.reshape(4, 2))
+                # if the contour has four vertices, then we have found
+                # the thermostat display
+                if len(approx) == 4:
+                        displayCnt = approx
+                        break
 
-# threshold the warped image, then apply a series of morphological
-# operations to cleanup the thresholded image
-thresh = cv2.threshold(warped, 0, 255,
-	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
-thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+        # extract the sign borders, apply a perspective transform
+        # to it
+        # A common task in computer vision and image processing is to perform 
+        # a 4-point perspective transform of a ROI in an image and obtain a top-down, "birds eye view" of the ROI
+        warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+        output = four_point_transform(image, displayCnt.reshape(4, 2))
 
-# find contours in the thresholded image, then initialize the
-# digit contours lists
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-digitCnts = []
+        # draw a red square on the image
+        cv2.drawContours(image, [displayCnt], -1, (0, 0, 255), 5)
 
-# loop over the digit area candidates
-for c in cnts:
-	# compute the bounding box of the contour
-	(x, y, w, h) = cv2.boundingRect(c)
+        # threshold the warped image, then apply a series of morphological
+        # operations to cleanup the thresholded image
+        # cv2.THRESH_OTSU. it automatically calculates a threshold value from image histogram 
+        # for a bimodal image
+        thresh = cv2.threshold(warped, 0, 255, 
+                cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+        thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
-	# if the contour is sufficiently large, it must be a digit
-	if w >= 15 and (h >= 30 and h <= 40):
-		digitCnts.append(c)
 
-# sort the contours from left-to-right, then initialize the
-# actual digits themselves
-digitCnts = contours.sort_contours(digitCnts,
-	method="left-to-right")[0]
-digits = []
+        # (roiH, roiW) = roi.shape
+        #subHeight = thresh.shape[0]/10
+        #subWidth = thresh.shape[1]/10
+        (subHeight, subWidth) = np.divide(thresh.shape, 10)
 
-# loop over each of the digits
-for c in digitCnts:
-	# extract the digit ROI
-	(x, y, w, h) = cv2.boundingRect(c)
-	roi = thresh[y:y + h, x:x + w]
+        # mark the ROIs borders on the image
+        cv2.rectangle(output, (subWidth, 4*subHeight), (3*subWidth, 9*subHeight), (0,255,0),2) # left block
+        cv2.rectangle(output, (4*subWidth, 4*subHeight), (6*subWidth, 9*subHeight), (0,255,0),2) # center block
+        cv2.rectangle(output, (7*subWidth, 4*subHeight), (9*subWidth, 9*subHeight), (0,255,0),2) # right block
+        cv2.rectangle(output, (3*subWidth, 2*subHeight), (7*subWidth, 4*subHeight), (0,255,0),2) # top block
 
-	# compute the width and height of each of the 7 segments
-	# we are going to examine
-	(roiH, roiW) = roi.shape
-	(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
-	dHC = int(roiH * 0.05)
+        # substract 4 ROI of the sign thresh image
+        leftBlock = thresh[4*subHeight:9*subHeight, subWidth:3*subWidth]
+        centerBlock = thresh[4*subHeight:9*subHeight, 4*subWidth:6*subWidth]
+        rightBlock = thresh[4*subHeight:9*subHeight, 7*subWidth:9*subWidth]
+        topBlock = thresh[2*subHeight:4*subHeight, 3*subWidth:7*subWidth]
 
-	# define the set of 7 segments
-	segments = [
-		((0, 0), (w, dH)),	# top
-		((0, 0), (dW, h // 2)),	# top-left
-		((w - dW, 0), (w, h // 2)),	# top-right
-		((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
-		((0, h // 2), (dW, h)),	# bottom-left
-		((w - dW, h // 2), (w, h)),	# bottom-right
-		((0, h - dH), (w, h))	# bottom
-	]
-	on = [0] * len(segments)
+        # we now track the fraction of each ROI
+        leftFraction = np.sum(leftBlock)/(leftBlock.shape[0]*leftBlock.shape[1])
+        centerFraction = np.sum(centerBlock)/(centerBlock.shape[0]*centerBlock.shape[1])
+        rightFraction = np.sum(rightBlock)/(rightBlock.shape[0]*rightBlock.shape[1])
+        topFraction = np.sum(topBlock)/(topBlock.shape[0]*topBlock.shape[1])
 
-	# loop over the segments
-	for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
-		# extract the segment ROI, count the total number of
-		# thresholded pixels in the segment, and then compute
-		# the area of the segment
-		segROI = roi[yA:yB, xA:xB]
-		total = cv2.countNonZero(segROI)
-		area = (xB - xA) * (yB - yA)
+        segments = (leftFraction, centerFraction, rightFraction, topFraction)
+        segments = tuple(1 if segment > 230 else 0 for segment in segments)
 
-		# if the total number of non-zero pixels is greater than
-		# 50% of the area, mark the segment as "on"
-		if total / float(area) > 0.5:
-			on[i]= 1
+        return SIGNS_LOOKUP[segments]
 
-	# lookup the digit and draw it on the image
-	digit = SIGNS_LOOKUP[tuple(on)]
-	digits.append(digit)
-	cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
-	cv2.putText(output, str(digit), (x - 10, y - 10),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
-
-# display the digits
-print(u"{}{}.{} \u00b0C".format(*digits))
-cv2.imshow("Input", image)
-cv2.imshow("Output", output)
-cv2.waitKey(0)
+print(defineTrafficSign(image))
